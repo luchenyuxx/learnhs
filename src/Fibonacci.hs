4f5d52185f341fe544f7febd0d4edb1d9321bd22
chenyu <chenyu.lu@density-tech.com>
2016-05-27 18:24:15 +0200

    part of exercise editor

--- src/Fibonacci.hs
+++ src/Fibonacci.hs
@@ -17,3 +17,19 @@ instance Show a => Show (Stream a) where
 
 streamToList :: Stream a -> [a]
 streamToList (Cons a b) = a : streamToList b
+
+streamRepeat :: a -> Stream a
+streamRepeat a = Cons a (streamRepeat a)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons a b) = Cons (f a) (streamMap f b)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f a = Cons a $ streamFromSeed f $ f a
+
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons a b) (Cons x y) =
+  Cons a $ Cons x $ interleaveStreams b y
